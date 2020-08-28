cc056da1f3e339e87ed4f9b284df699abb1d39e3
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2020-08-29 00:50:58 +0530

    feat:login card complete

--- client/app/src/components/Login/Login.tsx
+++ client/app/src/components/Login/Login.tsx
@@ -16,7 +16,21 @@ class Login extends Component<ILoginProps, ILoginState> {
 		return (
 			<Fragment>
 				<div className='App Login'>
-					<div className='Card'>Hello</div>
+					<div className='Card'>
+						<h1 style={{ textAlign: 'center' }}>Login</h1>
+						<form onSubmit={(e) => e.preventDefault()}>
+							<div className='input-wrapper'>
+								<input type='text' name='username' placeholder='Enter username.' />
+							</div>
+							<div className='input-wrapper'>
+								<input type='password' name='password' placeholder='Enter your password.' />
+							</div>
+
+							<div className='input-wrapper submit'>
+								<input type='submit' value='Submit' />
+							</div>
+						</form>
+					</div>
 				</div>
 			</Fragment>
 		)
