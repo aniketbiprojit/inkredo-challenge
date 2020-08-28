d8be858fc50dc3c0d99a75b8554bc764a0b1b917
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2020-08-29 02:44:27 +0530

    feat: login

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -8,16 +8,24 @@ const jwt = require('jsonwebtoken')
 const app = express.Router()
 
 app.post('/register', async (req, res) => {
-	// console.log(req.body)
+	console.log(req.body)
 	// console.log(await bcrypt.hash('req.body.password', 10))
 	// return
 	try {
 		if (req.body.user) {
 			if (req.body.user.username && req.body.user.password && req.body.user.email) {
+				if (await User.findOne({ username: req.body.user.username })) {
+					res.status(400).send('Username exists.')
+					return
+				}
+				if (await User.findOne({ email: req.body.user.email })) {
+					res.status(400).send('Email exists.')
+					return
+				}
 				req.body.password = await bcrypt.hash(req.body.user.password, 10)
 				let user = new User(req.body.user)
 				await user.save()
-
+				console.log(user)
 				user.password = ''
 				res.send(user)
 			} else {
@@ -40,19 +48,20 @@ app.post('/register', async (req, res) => {
 app.post('/login', async (req, res) => {
 	// console.log(jwt.verify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiaGVsbG8iLCJpYXQiOjE1OTg2NDY4NTMsImV4cCI6MTU5OTI1MTY1M30.LQyP5BpeXy7ifQ5UHDZDlcv4IEO0wjtUJ6ISILo2bVA','secret'))
 	// console.log(await bcrypt.compare('req.body.password','$2b$10$VtxJZdfSDEjQFF9TnCQyKOq7OFucwY7bpYWV45xfXMDNf.tMBzm6q'))
-	console.log(req.body)
+	// console.log(await User.find())
 	try {
 		if (req.body.user) {
 			if (req.body.user.username && req.body.user.password) {
-				let user = await User.findOne({ username: req.body.username })
+				let user = await User.findOne({ username: req.body.user.username })
 				if (user === null) {
 					res.status(400).send('User does not exist.')
 					return
 				}
-				let pass = await bcrypt.compare(req.body.password, user.password)
+				let pass = await bcrypt.compare(req.body.user.password, user.password)
+				// console.log(pass)
 				if (pass) {
-					let token = jwt.sign({ email: req.body.username }, 'secret', { expiresIn: '7d' })
-					res.send({ username: user, token: token })
+					let token = jwt.sign({ username: req.body.user.username }, 'secret', { expiresIn: '7d' })
+					res.send({ username: req.body.user.username, token: token })
 				} else {
 					res.status(403).send('Wrong Email or Password.')
 				}
